b5b7e75a06acf2ef1cc726ae30e999c14739037f
Mostafa Bakr <mostafabakr5505@gmail.com>
2026-04-08 21:24:13 +0200

    إضافة معالج لزر الرفع وصورة جديدة للمشروع تفاعل الواجهة (Event Handling): إضافة مستمع لحدث الضغط (Click Event) على زر "Start Upload"، يقوم بإظهار تنبيه (Alert) لمحاكاة عملية الانتقال إلى صفحة الرفع.
    إدارة الملفات: إضافة ملف صورة جديد بصيغة PNG باسم unnamed.png إلى ملفات المشروع.

---
+++ SmartAgricultuer/Controllers/UserPanel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartAgricultuer.Controllers
+{
+    public class UserPanel : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult u()
+        {
+            return View();
+        }
+    }
+}
---
+++ SmartAgricultuer/wwwroot/js/home.js
@@ -0,0 +1,4 @@
+// اضغط زر Start Upload
+document.getElementById('startUploadBtn').addEventListener('click', () => {
+  alert('Navigate to Upload Page (like navigate("/upload") in React)');
+});
